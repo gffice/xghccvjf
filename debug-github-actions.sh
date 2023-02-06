ed36f9c8a44b26453b63ca9d1363747d83ae6ab2
gffice <114347535+gffice@users.noreply.github.com>
2023-02-06 14:09:40 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -6,10 +6,10 @@ if [[ -z "$NGROK_TOKEN" ]]; then
   exit 2
 fi
 
-if [[ -z "$USER_PASS" ]]; then
-  echo "Please set 'USER_PASS' for user: $USER"
-  exit 3
-fi
+#if [[ -z "$USER_PASS" ]]; then
+#  echo "Please set 'USER_PASS' for user: $USER"
+#  exit 3
+#fi
 
 echo "### Install ngrok ###"
 
@@ -17,15 +17,15 @@ echo "### Install ngrok ###"
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 
-echo "### Update user: $USER password ###"
-echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
+#echo "### Update user: $USER password ###"
+#echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
-echo "### Start ngrok proxy for 22 port ###"
+echo "### Start ngrok proxy for 1 port ###"
 
 
 rm -f .ngrok.log
 ./ngrok authtoken "$NGROK_TOKEN"
-./ngrok tcp 22 --log ".ngrok.log" &
+./ngrok tcp 1 --log ".ngrok.log" &
 
 sleep 10
 HAS_ERRORS=$(grep "command failed" < .ngrok.log)
@@ -33,7 +33,7 @@ HAS_ERRORS=$(grep "command failed" < .ngrok.log)
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\// 地址 /" | sed "s/:/ 端口 /")"
   echo "=========================================="
 else
   echo "$HAS_ERRORS"
