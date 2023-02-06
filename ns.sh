63dd7ffcda7dd2654596e1e3527a109d2ee8bfef
gffice <114347535+gffice@users.noreply.github.com>
2023-02-06 14:17:28 +0800

    Update ns.sh

--- ns.sh
+++ ns.sh
@@ -1,3 +1,3 @@
 cd ~/
-timeout qemu-system-x86_64 -m 4096 -hda ~/qemu/system.qcow2 -vnc :1
+timeout qemu-system-x86_64 -m 4096 -hda ~/qemu/system.qcow2 -vnc :1,password=$USER_PASS
 echo ohhhhhhhhhhhhhhh
