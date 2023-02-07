5034da6b560c2a21af0c0a66a1606550d268d1e7
gffice <114347535+gffice@users.noreply.github.com>
2023-02-07 12:17:31 +0800

    Update nd.sh

--- nd.sh
+++ nd.sh
@@ -1,4 +1,5 @@
 cd ~/
+mkdir qemu
 #wget 'https://kzh4-my.sharepoint.com/:u:/g/personal/data_kzh4_onmicrosoft_com/EU1lxaTWv0VOg4MsGhPDkNsBOZW5ml-56MJA1y-PcACaSw?e=d6vkTK&download=1' -O ~/system.iso
 qemu-img create ~/qemu/system.qcow2 20G
 timeout 4h qemu-system-x86_64 -m 4096 -hda ~/qemu/system.qcow2 -cdrom ~/system.iso -vnc :1
