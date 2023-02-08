fb0a9b1b931e702b23a4bc5263d142c3f75b356f
gffice <114347535+gffice@users.noreply.github.com>
2023-02-08 22:46:30 +0800

    Update nd.sh

--- nd.sh
+++ nd.sh
@@ -2,7 +2,7 @@ cd ~/
 mkdir qemu
 #wget 'https://kzh4-my.sharepoint.com/:u:/g/personal/data_kzh4_onmicrosoft_com/EU1lxaTWv0VOg4MsGhPDkNsBOZW5ml-56MJA1y-PcACaSw?e=d6vkTK&download=1' -O ~/system.iso
 qemu-img create ~/qemu/system.qcow2 20G
-qemu-system-x86_64 -boot cd -hda ~/qemu/system.qcow2 -cdrom ~/system.iso -vnc :3111
+qemu-system-x86_64 -boot cd -hda ~/qemu/system.qcow2 -cdrom ~/system.iso -vnc :3111 & ss -ntl
 echo uhcbhj
 rm ~/system.iso
 echo ohhhhhhhhhhhhhhh
